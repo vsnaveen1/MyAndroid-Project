51c75d0049d5e72ae343e5ad861edd66975be3d9
Naveen Velicheti <vsnaveen1@gmail.com>
2015-10-31 22:21:10 -0400

    Create nishi.js
    This file is referred from server.js to connect to mongo db.

---
+++ nishi.js
@@ -0,0 +1,154 @@
+Last login: Sat Oct 24 11:29:52 on ttys000
+Srinivasas-MacBook-Pro:~ srinivasavelicheti$ ls -lrt
+total 40368
+drwxr-xr-x+  5 srinivasavelicheti  staff       170 Aug  6  2014 Public
+drwx------+  4 srinivasavelicheti  staff       136 Aug  9  2014 Music
+drwxr-xr-x   9 srinivasavelicheti  staff       306 Feb  7  2015 android-sdks
+drwx------+  4 srinivasavelicheti  staff       136 May 30 21:34 Movies
+drwxr-xr-x   8 srinivasavelicheti  staff       272 Jun  2 22:29 AndroidStudioProjects
+drwx------@ 53 srinivasavelicheti  staff      1802 Jul 23 19:39 Library
+drwxr-xr-x   9 srinivasavelicheti  staff       306 Aug  7 11:38 NetBeansProjects
+drwx------+ 11 srinivasavelicheti  staff       374 Sep 11 10:04 Pictures
+-rw-r--r--@  1 srinivasavelicheti  staff    281668 Oct  4 14:16 ZipCloud.exe
+-rw-r--r--   1 srinivasavelicheti  staff    175698 Oct  4 14:16 MacKeeper.3.5.1.pkg
+-rw-r--r--@  1 srinivasavelicheti  staff    452149 Oct  4 14:16 smg.dmg
+-rw-r--r--   1 srinivasavelicheti  staff   3010369 Oct  4 14:16 installium_wrapper_v96.dmg
+-rw-r--r--   1 srinivasavelicheti  staff  16741925 Oct  4 14:17 install_flash_player_osx.dmg
+drwx------   4 srinivasavelicheti  staff       136 Oct  4 14:22 Applications
+drwx------+ 17 srinivasavelicheti  staff       578 Oct 11 20:05 Documents
+drwx------+ 46 srinivasavelicheti  staff      1564 Oct 14 19:12 Desktop
+drwx------+ 93 srinivasavelicheti  staff      3162 Oct 24 13:55 Downloads
+Srinivasas-MacBook-Pro:~ srinivasavelicheti$ cd Desktop/
+Srinivasas-MacBook-Pro:Desktop srinivasavelicheti$ ls -lrt
+total 48136
+-rw-r--r--@  1 srinivasavelicheti  staff   2321797 Dec 26  2013 com.google.android.voicesearch-2.1.4-www.APK4Fun.com.apk
+drwxr-xr-x   8 srinivasavelicheti  staff       272 May  3 21:05 NishiNewPassport
+-rw-r--r--@  1 srinivasavelicheti  staff     15281 May 10 12:54 Invitation.docx
+-rw-r--r--   1 srinivasavelicheti  staff    410662 May 10 13:21 inviscanned010.pdf
+-rw-r--r--   1 srinivasavelicheti  staff    410892 May 10 13:23 inviscanned011.pdf
+-rw-r--r--@  1 srinivasavelicheti  staff  11893136 May 16 23:41 dataset.json
+drwxrwxrwx   4 srinivasavelicheti  staff       136 May 17 07:55 data
+-rw-r--r--@  1 srinivasavelicheti  staff     26105 May 17 09:03 Dyslexia.docx
+-rw-r--r--@  1 srinivasavelicheti  staff   1717180 May 19 20:19 NishitaMovva_VisaCopy.png
+-rw-r--r--@  1 srinivasavelicheti  staff     34541 May 31 10:37 Pencil.png
+-rw-r--r--@  1 srinivasavelicheti  staff     42892 May 31 10:37 Umbrella.png
+-rw-r--r--@  1 srinivasavelicheti  staff     34167 May 31 10:39 Hat.png
+-rw-r--r--@  1 srinivasavelicheti  staff     41596 May 31 10:39 Fridge.png
+-rw-r--r--@  1 srinivasavelicheti  staff     24571 Jun  1 21:29 startButton.jpg
+drwxr-xr-x   3 srinivasavelicheti  staff       102 Jun  8 17:48 OCR
+drwxr-xr-x   5 srinivasavelicheti  staff       170 Jun 19 14:45 MyAndroidApkfiles
+drwxr-xr-x   6 srinivasavelicheti  staff       204 Jun 28 14:08 GameDevelopmentDownloads
+-rw-r--r--@  1 srinivasavelicheti  staff    411320 Jul  1 15:41 LalithaMovva_Passport001.pdf
+-rw-r--r--@  1 srinivasavelicheti  staff    480951 Jul  1 15:43 LalithaMovva_Passport002.pdf
+-rw-r--r--@  1 srinivasavelicheti  staff    478195 Jul  1 15:44 LalithaMovva_Passport003.pdf
+-rw-r--r--@  1 srinivasavelicheti  staff    134220 Jul  8 19:34 proofoflossform_LalithaMovva_2.pdf
+-rw-r--r--@  1 srinivasavelicheti  staff    134285 Jul  8 19:36 proofoflossform_LalithaMovva_1.pdf
+-rw-r--r--@  1 srinivasavelicheti  staff    850272 Jul  8 19:43 proofoflossform_LalithaMovva.pdf
+-rw-r--r--@  1 srinivasavelicheti  staff    124511 Jul 13 21:39 Nishita_Week9_update_IHE 7990.docx
+-rw-r--r--@  1 srinivasavelicheti  staff     82930 Jul 20 22:10 INTRODUCTION TO JAVA PROGRAMMING.docx
+-rw-r--r--@  1 srinivasavelicheti  staff     75032 Jul 21 20:10 GifImageView.java
+-rw-r--r--@  1 srinivasavelicheti  staff    242352 Jul 21 20:10 GifDecoder.java
+drwxr-xr-x  11 srinivasavelicheti  staff       374 Jul 23 19:53 untitled folder
+-rw-r--r--@  1 srinivasavelicheti  staff     37242 Jul 27 20:52 derivatives.gif
+-rw-r--r--   1 srinivasavelicheti  staff    348121 Jul 28 15:39 U00747714_HealthForm 004.pdf
+drwxr-xr-x  17 srinivasavelicheti  staff       578 Sep  2 13:27 WSU
+drwxr-xr-x   8 srinivasavelicheti  staff       272 Sep  7 09:26 wincellar
+-rw-r--r--@  1 srinivasavelicheti  staff    901686 Oct  4 15:36 Screen Shot 2015-10-04 at 3.36.53 PM.png
+-rw-r--r--@  1 srinivasavelicheti  staff    901686 Oct  4 15:36 Screen Shot 2015-10-04 at 3.36.55 PM.png
+-rw-r--r--@  1 srinivasavelicheti  staff    676631 Oct  4 15:37 Screen Shot 2015-10-04 at 3.37.19 PM.png
+-rw-r--r--@  1 srinivasavelicheti  staff     31622 Oct 11 20:51 Screen Shot 2015-10-11 at 8.51.43 PM.png
+-rw-r--r--@  1 srinivasavelicheti  staff     96867 Oct 14 18:42 Screen Shot 2015-10-14 at 6.42.10 PM.png
+-rw-r--r--@  1 srinivasavelicheti  staff    288079 Oct 14 18:50 Screen Shot 2015-10-14 at 6.50.31 PM.png
+-rw-r--r--   1 srinivasavelicheti  staff      2058 Oct 14 19:03 Signature.jks
+-rw-r--r--   1 srinivasavelicheti  staff   1098629 Oct 14 19:04 app-release.apk
+-rw-r--r--@  1 srinivasavelicheti  staff    204673 Oct 14 19:12 Screen Shot 2015-10-14 at 7.12.26 PM.png
+drwxr-xr-x  15 srinivasavelicheti  staff       510 Oct 24 11:21 R
+Srinivasas-MacBook-Pro:Desktop srinivasavelicheti$ cd wincellar/
+Srinivasas-MacBook-Pro:wincellar srinivasavelicheti$ ls -lrt
+total 16
+-rw-r--r--   1 srinivasavelicheti  staff  175 May 24 23:52 package.json
+drwxr-xr-x   7 srinivasavelicheti  staff  238 May 25 01:39 node_modules
+-rw-r--r--   1 srinivasavelicheti  staff  458 Jun  4 20:35 server.js
+drwxr-xr-x  15 srinivasavelicheti  staff  510 Oct 14 17:59 routes
+Srinivasas-MacBook-Pro:wincellar srinivasavelicheti$ vi server.js
+Srinivasas-MacBook-Pro:wincellar srinivasavelicheti$ node.js server.js
+-bash: node.js: command not found
+Srinivasas-MacBook-Pro:wincellar srinivasavelicheti$ node server.js
+express deprecated app.configure: Check app.get('env') in an if statement server.js:6:5
+connect deprecated multipart: use parser (multiparty, busboy, formidable) npm module instead node_modules/express/node_modules/connect/lib/middleware/bodyParser.js:56:20
+connect deprecated limit: Restrict request size at location of read node_modules/express/node_modules/connect/lib/middleware/multipart.js:86:15
+Listening on port 3000...
+Connected to 'NishitaDB' database
+^C
+Srinivasas-MacBook-Pro:wincellar srinivasavelicheti$ ls -lrt
+total 16
+-rw-r--r--   1 srinivasavelicheti  staff  175 May 24 23:52 package.json
+drwxr-xr-x   7 srinivasavelicheti  staff  238 May 25 01:39 node_modules
+-rw-r--r--   1 srinivasavelicheti  staff  458 Jun  4 20:35 server.js
+drwxr-xr-x  15 srinivasavelicheti  staff  510 Oct 14 17:59 routes
+Srinivasas-MacBook-Pro:wincellar srinivasavelicheti$ cd routes/
+Srinivasas-MacBook-Pro:routes srinivasavelicheti$ ls -lrt
+total 608
+-rw-r--r--  1 srinivasavelicheti  staff   2196 May 28 22:01 Nishi.jks
+-rw-r--r--@ 1 srinivasavelicheti  staff  34541 May 31 10:37 Pencil.png
+-rw-r--r--@ 1 srinivasavelicheti  staff  42892 May 31 10:37 Umbrella.png
+-rw-r--r--@ 1 srinivasavelicheti  staff  34167 May 31 10:39 Hat.png
+-rw-r--r--@ 1 srinivasavelicheti  staff  41596 May 31 10:39 Refrigerator.png
+-rw-r--r--@ 1 srinivasavelicheti  staff   1514 Jun  4 20:02 winess.js
+-rw-r--r--  1 srinivasavelicheti  staff   2848 Jun  4 20:32 wines.js
+-rw-r--r--@ 1 srinivasavelicheti  staff   2232 Jun  4 20:40 elephant.png
+drwxr-xr-x  6 srinivasavelicheti  staff    204 Jun  7 07:12 downloadedHandwriting
+-rw-r--r--@ 1 srinivasavelicheti  staff  42985 Jul 20 21:07 multiplication.gif
+-rw-r--r--  1 srinivasavelicheti  staff   1280 Jul 20 21:09 nishi.js
+-rw-r--r--@ 1 srinivasavelicheti  staff  79997 Jul 27 21:24 derivatives.gif
+Srinivasas-MacBook-Pro:routes srinivasavelicheti$ vi nishi.js
+
+db = new Db('NishitaDB', server);
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'NishitaDB' database");
+            }
+
+});
+
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving wine: ' + id);
+    db.collection('Level1QuestionCollections', function(err, collection) {
+        collection.findOne({'_id':id}, function(err, item) {
+res.send(item);
+        });
+    });
+};
+
+exports.findByName = function(req,res){
+
+var name = req.params.name;
+console.log('the name in the request: ' + name);
+db.collection('wines',function(err,collection){
+collection.findOne({'name':name},function(err,item){
+console.log('After responding');
+console.log('retrieved wine ' + name);
+
+
+
+res.send(item);
+
+
+});
+});
+
+};
+
+exports.displayImage = function(req, res) {
+
+var imageLoc = req.params.image;
+    console.log('Retrieving image: ' + imageLoc);
+var img = fs.readFileSync('/Users/srinivasavelicheti/Desktop/wincellar/routes/'+imageLoc+'.gif');
+res.writeHead(200, {'Content-Type': 'image/gif' });
+     res.end(img, 'binary');
+
+
+};
+
